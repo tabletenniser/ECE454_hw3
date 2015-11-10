b92ca275a13693a23c1f78526762150b7da50f9b
tabletenniser@gmail.com <tabletenniser@gmail.com>
2015-11-10 00:18:23 -0500

    2 failure, 15/60 for util, 33/40 for throughput

--- assn/mm.c
+++ assn/mm.c
@@ -42,7 +42,9 @@ team_t team = {
 #define WSIZE       sizeof(void *)            /* word size (bytes) */
 #define DSIZE       (2 * WSIZE)            /* doubleword size (bytes) */
 /* #define CHUNKSIZE   (1<<7)      #<{(| initial heap size (bytes) = 128 bytes|)}># */
-#define CHUNKSIZE   (1<<3)      /* initial heap size (bytes) = 128 bytes*/
+#define CHUNKSIZE   (1<<4)      /* initial heap size (bytes) = 16 bytes, two words*/
+#define NUM_OF_FREE_LISTS 30
+#define SHIFT_SIZE 2
 
 #define MAX(x,y) ((x) > (y)?(x) :(y))
 
@@ -65,10 +67,15 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
-#define LOGGING_LEVEL 1
+#define PREV_FREE_BLKP(bp)  ((char*)bp)
+#define NEXT_FREE_BLKP(bp)  ((char*)bp + WSIZE)
+
+#define LOGGING_LEVEL 0
+/* #define LOGGING_LEVEL 6 */
 #define logg(level, args ...)    if(level <= LOGGING_LEVEL){ printf(args); printf("\n"); fflush(stdout);}
 
-void* heap_listp = NULL;
+void* heap_listp = NULL;        // Global heap pointer
+void* free_block_lists[NUM_OF_FREE_LISTS];     // An array of free blocks organized by sizes
 
 /* Logging utility to bypass the stdout buffer, print out the message immediately */
 /* void logg(int level, char *arg, ...){ */
@@ -79,6 +86,78 @@ void* heap_listp = NULL;
 /* 	} */
 /* } */
 
+/**********************************************************
+ * mm_check
+ * Check the consistency of the memory heap
+ * Return nonzero if the heap is consistant.
+ *********************************************************/
+int mm_check(void){
+    int i = 0;
+    char *iter;
+    int fail = 0;
+    for (i = 0; i<NUM_OF_FREE_LISTS; i++){
+        iter = free_block_lists[i];
+        /* while (iter!=NULL) { */
+        if (iter!=NULL) {
+            if (GET(FTRP(iter)) != GET(HDRP(iter))) {
+                printf("index: %d; bp: %p; header: %zx; footer: %zx", i, iter, GET(HDRP(iter)), GET(FTRP(iter)));
+               fail = 1;
+               break;
+            }
+            if (GET_ALLOC(HDRP(iter))) {
+                printf("index: %d; bp: %p; header: %zx", i, iter, GET(HDRP(iter)));
+                fail = 1;
+                break;
+            }
+            if (GET_ALLOC(FTRP(iter))) {
+                printf("index: %d; bp: %p; footer: %zx", i, iter, GET(FTRP(iter)));
+                fail = 1;
+                break;
+            }
+            iter = (char *)GET(NEXT_FREE_BLKP(iter));
+            if (iter!=NULL){
+                if (GET(FTRP(iter)) != GET(HDRP(iter))) {
+                    printf("index: %d; bp: %p; header: %zx; footer: %zx", i, iter, GET(HDRP(iter)), GET(FTRP(iter)));
+                    fail = 1;
+                    break;
+                }
+                if (GET_ALLOC(HDRP(iter))) {
+                    printf("index: %d; bp: %p; header: %zx", i, iter, GET(HDRP(iter)));
+                    fail = 1;
+                    break;
+                }
+                if (GET_ALLOC(FTRP(iter))) {
+                    printf("index: %d; bp: %p; footer: %zx", i, iter, GET(FTRP(iter)));
+                    fail = 1;
+                    break;
+                }
+            }
+        }
+    }
+    if (fail == 1){
+        printf("************** mm_check() FAILS!!!!!! ***********");
+        return 1;
+    }
+    return 0;
+}
+
+void print_free_lists(){
+    int i = 0;
+    char *iter;
+    printf("========== The free block lists ==========\n");
+    for (i = 0; i<NUM_OF_FREE_LISTS; i++){
+        iter = free_block_lists[i];
+        printf("%d: ", i);
+        /* while (iter!=NULL) { */
+        if (iter!=NULL) {
+            printf("%p\t", iter);
+            iter = (char *)GET(NEXT_FREE_BLKP(iter));
+            if (iter!=NULL)
+                printf("%p\t", iter);
+        }
+        printf("\n");
+    }
+}
 
 /**********************************************************
  * mm_init
@@ -97,9 +176,68 @@ int mm_init(void)
     PUT(heap_listp + (3 * WSIZE), PACK(0, 1));    // epilogue header
     heap_listp += DSIZE;
     logg(1, "initial heap_listp: %p", heap_listp);
-    logg(2, "============ mm_init() ends ==============");
+    // Initialize the segregated free lists.
+    int i;
+    for (i = 0; i < NUM_OF_FREE_LISTS; i++)
+        free_block_lists[i]=NULL;
+    logg(3, "============ mm_init() ends ==============");
 
     return 0;
+}
+
+void add_free_block(void *bp){
+    logg(4, "============ add_free_block() starts ==============");
+    int free_list_i = 0;
+    int size = GET_SIZE(HDRP(bp));
+    while (size > (1 << (free_list_i+SHIFT_SIZE))){
+        free_list_i++;
+    }
+    if (free_block_lists[free_list_i])
+        PUT(PREV_FREE_BLKP(free_block_lists[free_list_i]), (uintptr_t)bp);
+    PUT(NEXT_FREE_BLKP(bp), (uintptr_t)free_block_lists[free_list_i]);
+    PUT(PREV_FREE_BLKP(bp), (uintptr_t)NULL);
+    free_block_lists[free_list_i]=bp;
+    logg(5, "free_list_i is: %d; size is: %d; bp is: %p", free_list_i, size, bp);
+    logg(5, "next of bp is: %zx; prev of bp is: %zx", GET(NEXT_FREE_BLKP(bp)), GET(PREV_FREE_BLKP(bp)));
+    logg(4, "============ add_free_block() ends ==============");
+    return;
+}
+
+// TODO: COMMENT
+void remove_free_block(void *bp){
+    logg(4, "============ remove_free_block() starts ==============");
+    logg(5, "bp: %p;prev blk ptr: %zx;next blk ptr: %zx", bp, GET(PREV_FREE_BLKP(bp)), GET(NEXT_FREE_BLKP(bp)));
+    int free_list_i = 0;
+    int size = GET_SIZE(HDRP(bp));
+    while (size > (1 << (free_list_i+SHIFT_SIZE))){
+        free_list_i++;
+    }
+    char *next_block_ptr = (char *)GET(NEXT_FREE_BLKP(bp));
+    char *prev_block_ptr = (char *)GET(PREV_FREE_BLKP(bp));
+    // Case for only one free block
+    if (!GET(PREV_FREE_BLKP(bp)) && !GET(NEXT_FREE_BLKP(bp))){
+        logg(5, "Case A: block is the only free block in the list");
+        free_block_lists[free_list_i] = NULL;
+    }
+    // Case where bp is the first free block
+    else if (!GET(PREV_FREE_BLKP(bp)) && GET(NEXT_FREE_BLKP(bp))){
+        logg(5, "Case B: block is the first free block in the list");
+        PUT(PREV_FREE_BLKP(next_block_ptr), (uintptr_t)NULL);
+        free_block_lists[free_list_i] = next_block_ptr;
+    }
+    // Case where bp is the last free block
+    else if (GET(PREV_FREE_BLKP(bp)) && !GET(NEXT_FREE_BLKP(bp))){
+        logg(5, "Case C: block is the last free block in the list");
+        PUT(NEXT_FREE_BLKP(prev_block_ptr), (uintptr_t)NULL);
+    }
+    // Case where free blocks exist both before and after bp.
+    else {
+        logg(5, "Case D: free block exists in both directions");
+        PUT(NEXT_FREE_BLKP(prev_block_ptr), (uintptr_t)NULL);
+        PUT(PREV_FREE_BLKP(next_block_ptr), (uintptr_t)NULL);
+    }
+    logg(4, "============ remove_free_block() ends ==============");
+    return;
 }
 
 /**********************************************************
@@ -112,35 +250,45 @@ int mm_init(void)
  **********************************************************/
 void *coalesce(void *bp)
 {
+    logg(4, "============ coalesce() starts ==============");
+    logg(1, "coalesce() called with bp: %p; Previous block: %p header: %zx; Next block: %p header: %zx", bp, PREV_BLKP(bp), GET(HDRP(PREV_BLKP(bp))), NEXT_BLKP(bp), GET(HDRP(NEXT_BLKP(bp))));
+
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
     if (prev_alloc && next_alloc) {       /* Case 1 */
-        return bp;
+        logg(2, "Case 1: Both prev and next blocks are allocated. NO coalescing.");
     }
-
     else if (prev_alloc && !next_alloc) { /* Case 2 */
+        logg(2, "Case2: Next block is free.");
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        remove_free_block(NEXT_BLKP(bp));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-        return (bp);
     }
-
     else if (!prev_alloc && next_alloc) { /* Case 3 */
+        logg(2, "Case3: Prev block is free.");
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        remove_free_block(PREV_BLKP(bp));
+        bp = PREV_BLKP(bp);     // move bp one block ahead
+        PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        return (PREV_BLKP(bp));
+    }
+    else {            /* Case 4 */
+        logg(2, "Case4: Both blocks are free.");
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)))  + GET_SIZE(FTRP(NEXT_BLKP(bp)))  ;
+        remove_free_block(NEXT_BLKP(bp));
+        remove_free_block(PREV_BLKP(bp));
+        bp = PREV_BLKP(bp);
+        PUT(HDRP(bp), PACK(size,0));
+        PUT(FTRP(bp), PACK(size,0));
     }
 
-    else {            /* Case 4 */
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)))  +
-            GET_SIZE(FTRP(NEXT_BLKP(bp)))  ;
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size,0));
-        return (PREV_BLKP(bp));
-    }
+    // Add the bp block to the beginning of free list of corresponding size.
+    add_free_block(bp);
+    logg(4, "============ coalesce() ends ==============");
+    return bp;
 }
 
 /**********************************************************
@@ -164,9 +312,11 @@ void *extend_heap(size_t words)
     PUT(HDRP(bp), PACK(size, 0));                // free block header
     PUT(FTRP(bp), PACK(size, 0));                // free block footer
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));        // new epilogue header
+    add_free_block(bp);
+    return bp;
 
     /* Coalesce if the previous block was free */
-    return coalesce(bp);
+    /* return coalesce(bp); */
 }
 
 
@@ -179,15 +329,29 @@ void *extend_heap(size_t words)
 void * find_fit(size_t asize)
 {
     void *bp;
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
-    {
-        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
-        {
-            logg(1, "find_fit() finds a fittable block at: %p for size: %zx(h)%zu(d)", bp, asize, asize);
-            return bp;
+    int free_list_i=0;
+    /* for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) */
+    /* { */
+    /*     if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) */
+    /*     { */
+    /*         logg(1, "find_fit() finds a fittable block at: %p for size: %zx(h)%zu(d)", bp, asize, asize); */
+    /*         return bp; */
+    /*     } */
+    /* } */
+    for (free_list_i = 0; free_list_i<NUM_OF_FREE_LISTS; free_list_i++){
+        // 1<<(free_list_i+2) so that free_block_lists[0] contains the free blocks of size 4 words.
+        if ((1<<(free_list_i+SHIFT_SIZE-1))<asize){
+            continue;
+        }
+        bp = free_block_lists[free_list_i];
+        while (bp != NULL){
+            if (asize <= GET_SIZE(HDRP(bp))){
+                logg(1, "find_fit() finds a fittable block at: %p for size: %zx(h)%zu(d)", bp, asize, asize);
+                return bp;
+            }
         }
     }
-    logg(1, "find_fit() cannot find a free block with proper size.");
+    logg(2, "find_fit() cannot find a free block with proper size: %zx(h)%zu(d).", asize, asize);
     return NULL;
 }
 
@@ -197,6 +361,7 @@ void * find_fit(size_t asize)
  **********************************************************/
 void place(void* bp, size_t asize)
 {
+    remove_free_block(bp);
   /* Get the current block size */
   size_t bsize = GET_SIZE(HDRP(bp));
 
@@ -210,8 +375,9 @@ void place(void* bp, size_t asize)
  **********************************************************/
 void mm_free(void *bp)
 {
-    logg(2, "============ mm_free() starts ==============");
-	logg(1, "mm_free() with bp: %p; header: %x(h); footer: %x(h)", bp, GET(HDRP(bp)), GET(FTRP(bp)));
+    logg(3, "============ mm_free() starts ==============");
+	logg(1, "mm_free() with bp: %p; header: %zx(h); footer: %zx(h)", bp, GET(HDRP(bp)), GET(FTRP(bp)));
+    mm_check();
     if(bp == NULL){
       return;
     }
@@ -219,7 +385,7 @@ void mm_free(void *bp)
     PUT(HDRP(bp), PACK(size,0));
     PUT(FTRP(bp), PACK(size,0));
     coalesce(bp);
-    logg(2, "============ mm_free() ends ==============");
+    logg(3, "============ mm_free() ends ==============");
 }
 
 
@@ -233,7 +399,8 @@ void mm_free(void *bp)
  **********************************************************/
 void *mm_malloc(size_t size)
 {
-    logg(2, "============ mm_malloc() starts ==============");
+    mm_check();
+    logg(3, "============ mm_malloc() starts ==============");
     size_t asize; /* adjusted block size */
     size_t extendsize; /* amount to extend heap if no fit */
     char * bp;
@@ -247,6 +414,7 @@ void *mm_malloc(size_t size)
         asize = 2 * DSIZE;
     else
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1))/ DSIZE);
+    asize += DSIZE;     // For the prev / next free block.
 
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {
@@ -256,11 +424,14 @@ void *mm_malloc(size_t size)
 
     /* No fit found. Get more memory and place the block */
     extendsize = MAX(asize, CHUNKSIZE);
+    mm_check();
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
         return NULL;
+    mm_check();
     place(bp, asize);
     logg(1, "mm_malloc(%zx(h)%zu(d)) returns bp: %p; with actual size: %zx", size, size, bp, asize);
-    logg(2, "============ mm_malloc() ends ==============");
+    logg(3, "============ mm_malloc() ends ==============");
+    mm_check();
     return bp;
 
 }
@@ -283,6 +454,9 @@ void *mm_realloc(void *ptr, size_t size)
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
+    size_t oldSize = GET_SIZE(HDRP(oldptr));
+    if (2*DSIZE + size < oldSize)
+        return oldptr;
 
     newptr = mm_malloc(size);
     if (newptr == NULL)
@@ -295,13 +469,4 @@ void *mm_realloc(void *ptr, size_t size)
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
-}
-
-/**********************************************************
- * mm_check
- * Check the consistency of the memory heap
- * Return nonzero if the heap is consistant.
- *********************************************************/
-int mm_check(void){
-  return 1;
 }
